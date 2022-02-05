7dcf998712b17fc415beb39b19d8e632ef6a4612
fr0sty99 <j.neber@gmx.ch>
2022-02-06 00:20:38 +0100

    fixed cards css, fixed animations, detail weather card finished

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { SimpleWeatherCard } from './Components/CurrentWeather';
+import { SimpleWeatherCard, DetailWeatherCard } from './Components/CurrentWeather';
 import { GET_CURRENT_WEATHER } from './GraphQL/Queries';
 import './App.css';
 import Graph from './Components/Graph';
 
+// BarLoader
 import { BarLoader } from 'react-spinners';
 import { css } from "@emotion/react";
-
 const override = css`
   display: block;
   margin: 0 auto;
-  border-color: red;
+  position: absolute;
+  top:  50%;
+  left: 25%;
 `;
 
-const App = () => {
-  const { data, loading, error } = useQuery(GET_CURRENT_WEATHER);
-  var content;
+function App() {
+  const [detailActive, setDetailActive] = useState(false);
+  const { error, loading, data } = useQuery(GET_CURRENT_WEATHER);
+
   if (loading) {
-    content = <div><BarLoader css={override} size={150} color={"#123abc"} loading={true} speedMultiplier={1.5} /></div>;
+    return <div style={{ height: '100vh' }}><BarLoader css={override} color={"#123abc"} width={'50%'} height={20} loading={true} speedMultiplier={1.5} /></div>;
   }
   if (error) {
-    content = <div> Something went wrong..Oops</div>;
+    return <div> Something went wrong..Oops</div>;
   }
 
   if (data) {
-    content = <SimpleWeatherCard data={data} loading={loading} error={error} />
-  }
-
-  if (data || loading || error) {
     return (
       <>
         <div className='container-fluid' >
           <div id='title'>Wetter in Basel</div>
-          {content}
+          <SimpleWeatherCard data={data} error={error} onClick={() => { setDetailActive(!detailActive) }} />
+          <DetailWeatherCard data={data} detailActive={detailActive} />
           <div id='sub'>7 Tage Wetter in Basel</div>
           <div id='card1'>Tomorrow</div>
           <div id='card2'>TODO</div>
@@ -43,10 +43,6 @@ const App = () => {
           <div id='card6'>TODO</div>
           <div id='card7'>TODO</div>
           <div id='card8'>The day in a week</div>
-          {/* <h1>Aktuelles Wetter in Basel</h1>
-          <CurrentWeather />
-          <h1>7-Tage-Wetter</h1>
-          <WeatherForecast /> */}
         </div>
         <div id='sub2'>Graph</div>
         <Graph></Graph>
--- app/src/Components/CurrentWeather.js
+++ app/src/Components/CurrentWeather.js
@@ -1,78 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, Card } from 'react-bootstrap';
 
+export function SimpleWeatherCard(props) {
+    if (props.data) {
+        const obj = props.data.getCityByName.weather;
 
-export class SimpleWeatherCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isMoreInfoActive: false,
-            data: props.data,
-            loading: props.loading,
-            error: props.error,
+        if (obj) {
+            var iconCode = obj.summary.icon;
+            var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+            return (
+                <Card id='simpleweather'>
+                    <Card.Title>Übersicht</Card.Title>
+                    <Card.Body className='row'>
+                        <img src={iconUrl}
+                            alt='Icon'
+                        />
+                        <ul>
+                            <li>{obj.summary.description}</li>
+                            <li>Temperatur: {obj.temperature.actual} °C</li>
+                            <li>Tagestief: {obj.temperature.min} °C</li>
+                            <li>Tageshoch: {obj.temperature.max} °C</li>
+                        </ul>
+                        <Button onClick={() => props.onClick()}>More Info</Button>
+                    </Card.Body >
+                </Card >
+            );
         }
     }
+};
 
-    handleClick() {
-        this.setState({ isMoreInfoActive: !this.state.isMoreInfoActive });
+export function DetailWeatherCard(props) {
+    if (props.data) {
+        const obj = props.data.getCityByName.weather;
+        var className = props.detailActive ? 'visible' : 'hidden';
+        return (
+            <Card id='detailweather' className={className}>
+                <Card.Title>Details</Card.Title>
+                <Card.Body className='row'>
+                    <ul>
+                        <li>Windgeschwindigkeit: {obj.wind.speed} km/h</li>
+                        <li>Luftfeuchtigkeit: {obj.clouds.humidity} %</li>
+                        <li>Luftdruck: 1018.9 hPa</li>
+                        <li>Sonnenaufgang: 07:50</li>
+                        <li>Sonnenuntergang: 17:28</li>
+                    </ul>
+                </Card.Body >
+            </Card>
+        )
     }
-
-    render() {
-        if (this.state.data) {
-            const obj = this.state.data.getCityByName.weather;
-
-            if (obj) {
-                var iconCode = obj.summary.icon;
-                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-                return (
-                    <div id='simpleweather'>
-                        <div id='currentweather'>
-                            <Card>
-                                <Card.Title><h2>Übersicht</h2></Card.Title>
-                                <Card.Body className='row'>
-                                    <div>
-                                        <Card.Img
-                                            variant="top"
-                                            src={iconUrl}
-                                            alt='Wetter'
-                                        />
-
-                                        <ul>
-                                            <li>{obj.summary.description}</li>
-                                            <li>Temperatur: {obj.temperature.actual} °C</li>
-                                            <li>Tagestief: {obj.temperature.min} °C</li>
-                                            <li>Tageshoch: {obj.temperature.max} °C</li>
-                                        </ul>
-
-                                        <Button style={{ marginBottom: '1rem' }} onClick={() => this.handleClick()}>{this.state.isMoreInfoActive ? 'Less Info' : 'More Info'}</Button>
-                                    </div>
-                                </Card.Body >
-                            </Card >
-                        </div>
-                        <div id='details'>
-                            {this.state.isMoreInfoActive ? (
-                                <div>
-                                    <Card>
-                                        <Card.Title><h2>Details</h2></Card.Title>
-                                        <Card.Body className='row'>
-                                            <ul>
-                                                <li>Windgeschwindigkeit: {obj.wind.speed} km/h</li>
-                                                <li>Luftfeuchtigkeit: {obj.clouds.humidity} %</li>
-                                                <li>Luftdruck: 1018.9 hPa</li>
-                                                <li>Sonnenaufgang: 07:50</li>
-                                                <li>Sonnenuntergang: 17:28</li>
-                                            </ul>
-                                        </Card.Body >
-
-                                    </Card>
-                                </div>
-                            ) : ''}
-                        </div>
-                    </div>
-                );
-            }
-        }
-    }
-}
+};
